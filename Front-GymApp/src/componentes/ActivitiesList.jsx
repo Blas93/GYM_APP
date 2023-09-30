1a38aa12bcc8613092424edb827d2263d30a148b
Blas Vega <blasvega1993@gmail.com>
2023-09-30 23:37:46 +0100

    añadidos a la pagina de actividades, del registro

--- Front-GymApp/src/componentes/ActivitiesList.jsx
+++ Front-GymApp/src/componentes/ActivitiesList.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+/*import React from 'react'*/
 import { Activity } from './Activity'
+import { AddActivity } from '../pages/AddActivity'
+import { DeleteInformationActivityPage } from '../pages/DeleteInformationActivityPage'
+import {EditInformationActivityPage } from '../pages/EditInformationActivityPage'
 
 export const ActivitiesList = ({activities}) => {
   return (
     <section>
       <h2>Lista de Actividades</h2>
+      <DeleteInformationActivityPage DeleteInformationActivityPage={DeleteInformationActivityPage}/>
+      <AddActivity AddActivity={AddActivity} />
+      <EditInformationActivityPage EditInformationActivityPage={EditInformationActivityPage}/>
+      
       {activities.length ? (
           <ul>
             {activities.map((activity) => {
               <li key={activity.id}>
                 <Activity activity={activity} />
+                
               </li>
             })}
           </ul>
@@ -19,18 +27,3 @@ export const ActivitiesList = ({activities}) => {
     </section>
   )
 }
-
-//import { Activity } from "./Activity";
-
-//export const ActivityList = ({Activity}) => {
-  //  <ul>
-      //  {Activity.map{(Activity)}} =>(
-          //  <li key={Activity.id}>
-                //<Activity activity={activity} />
-          //  </li>
-       // )}
-    //</ul>
-  //  ) : (
-     //   <p>No hay ninguna actividad todavía</p>
-    //);
-//};
--- Front-GymApp/src/pages/AddActivity.jsx
+++ Front-GymApp/src/pages/AddActivity.jsx
@@ -1,29 +1,72 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../Css/AddActivity.css';
 
 export const AddActivity = () => {
   const [actividades, setActividades] = useState([]);
   const [nuevaActividad, setNuevaActividad] = useState('');
-
+  const [nombre, setNombre] = useState('');
+  const [descripcion, setDescripcion] = useState('');
+  const [tipologia, setTipologia] = useState('');
+  const [grupoMuscular, setGrupoMuscular] = useState('');
+  
+  
   const agregarActividad = () => {
     if (nuevaActividad.trim() === '') {
       return;
     }
 
-    setActividades([...actividades, nuevaActividad]);
+    setActividades([...actividades, nuevaActividad,]);
     setNuevaActividad('');
+    setNombre('');
+    setDescripcion('');
+    setTipologia('');
+    setGrupoMuscular('');
   };
+
+  useEffect(() => {
+    fetch('http://localhost3000/db/activities.js')
+    .then((response) => response.json())
+    .then((data) => {
+      setActividades(data);
+    })
+    .catch((error) => {
+      console.error('Error al obtener datos:', error);
+    });
+  },
+  );
+  
 
   return (
     <div>
       <h1>Ejercicio a Añadir </h1>
+      <button id = "agregar"  onClick={agregarActividad}>Agregar</button>
+      
       <input
         type="text"
-        placeholder="Agregar actividad"
-        value={nuevaActividad}
-        onChange={(e) => setNuevaActividad(e.target.value)}
+        placeholder="Nombre"
+        value={nombre}
+        onChange={(e) => setNombre(e.target.value)}
       />
-      <button onClick={agregarActividad}>Agregar</button>
+      <input
+        type="text"
+        placeholder="Descripción"
+        value={descripcion}
+        onChange={(e) => setDescripcion(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Tipología"
+        value={tipologia}
+        onChange={(e) => setTipologia(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Grupo Muscular"
+        value={grupoMuscular}
+        onChange={(e) => setGrupoMuscular(e.target.value)}
+      />
+     
+      
       <ul>
         {actividades.map((actividad, index) => (
           <li key={index}>{actividad}</li>

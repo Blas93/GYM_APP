c08e5688e2a46b7ad980a4eba21a090776caee09
Blas Vega <blasvega1993@gmail.com>
2023-10-28 23:44:43 +0100

    mejorado visual de la lista de actividades

--- Front-GymApp/src/componentes/Header.jsx
+++ Front-GymApp/src/componentes/Header.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom"; 
-
+import '../Css/Header.css';
 
 <logo />
 export const Header = () => {
     return (
         <header>
                 <a href ="/" className="logo-img">
-                    <img src="logo.png" alt="Logo de la aplicación"/>
+                    <img src="/logogymapp.png" alt="Logo de la aplicación"/>
                 </a>  
             <h2 className="logo-nombre">
             <Link to ="/">
